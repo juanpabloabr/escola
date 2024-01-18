8965c0052a864d0c5d361c9ba6e9a95d831c11e8
Juan Nogueira <juanpabloabr@gmail.com>
2024-01-18 16:06:35 -0400

    Adding POST Method

--- cursos/views.py
+++ cursos/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
@@ -16,6 +17,12 @@ class CursoAPIView(APIView):
         serializer = CursoSerializer(cursos, many = True)
         return Response(serializer.data)
     
+    def post(self,request):
+        serializer =  CursoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
 class AvaliacaoAPIView(APIView):
 
     """
@@ -26,4 +33,10 @@ class AvaliacaoAPIView(APIView):
         avaliacoes = Avaliacao.objects.all()
         serializer = AvaliacaoSerializer(avaliacoes, many = True)
         return Response(serializer.data)
+    
+    def post(self,request):
+        serializer =  AvaliacaoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
     
