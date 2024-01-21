ba432fe2ca6c57a42181b386551ba939d3d30b75
Juan Pablo <juanpabloabr@gmail.com>
2024-01-21 19:29:38 -0400

    Adding Pagination

--- escola/settings.py
+++ escola/settings.py
@@ -135,6 +135,9 @@ REST_FRAMEWORK =  {
     'DEFAULT_PERMISSION_CLASSES':(
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 3
+
 }
 
 # Default primary key field type
